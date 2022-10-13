d2601a37c960348ff3d8d08acab0b3918a46af69
GUSTAVO GOMES GONCALVES <gustavo.ggoncalves1@senacsp.edu.br>
2022-10-13 17:30:44 -0300

    cadastro

---
+++ app/Http/Controllers/CaminhaoController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\Caminhao;
+use Illuminate\Support\Facades\Redirect;
+
+class CaminhaoController extends Controller
+{
+    public function FormularioCadastro(){
+        return view('cadastrarCaminhao');
+    }
+
+    public function Editar(){
+        return view('editarCaminhao');
+
+    }
+
+    public function SalvarBanco(Request $request){
+        $dadosCaminhao = $request->validate([
+            'modelo'=>'string|required',
+            'marca'=>'string|required',
+            'ano'=>'string|required',
+            'cor'=>'string|required',
+            'valor'=>'string|nullable',
+        ]);
+
+        Caminhao::create($dadosCaminhao);
+        return Redirect::route('home');
+
+    }
+};
